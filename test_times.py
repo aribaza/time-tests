6a8f88e383725440ed0e13531060c7ad32608500
d-eniz <zcemdte@ucl.ac.uk>
2024-10-28 10:54:51 +0000

    Added negative test

--- test_times.py
+++ test_times.py
@@ -1,29 +1,33 @@
-from times import *
+from times import time_range, compute_overlap_time
+import pytest
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short) 
+    result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
 def test_no_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
-    result = compute_overlap_time(large, short) 
+    result = compute_overlap_time(large, short)
     expected = []
     assert result == expected
 
 def test_several_intervals():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short) 
+    result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
 def test_same_time_range():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    result = compute_overlap_time(large, short) 
+    result = compute_overlap_time(large, short)
     expected = [("2010-01-12 10:00:00", "2010-01-12 12:00:00")]
     assert result == expected
+
+def test_time_raise_backwards():
+    pytest.raises(ValueError, match="End time is before start time")
