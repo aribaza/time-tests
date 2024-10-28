7c63593201132d13689d7d03243241540acd5447
aribaza <aribazaga@gmail.com>
2024-10-28 09:48:18 +0000

    Added unit tests for 4 different scenarios

--- test_times.py
+++ test_times.py
@@ -6,3 +6,24 @@ def test_given_input():
     result = compute_overlap_time(large, short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
+    result = compute_overlap_time(large, short) 
+    expected = []
+    assert result == expected
+
+def test_several_intervals():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large, short) 
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_same_time_range():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    result = compute_overlap_time(large, short) 
+    expected = [("2010-01-12 10:00:00", "2010-01-12 12:00:00")]
+    assert result == expected
